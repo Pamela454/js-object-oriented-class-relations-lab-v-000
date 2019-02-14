46d794b034da08e5dc18d63c4e8863e085b67d86
Pamela Frieze <pfrieze@gmail.com>
2019-02-14 18:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Driver {
   
   passengers() {
     return store.passengers.filter(
-            function(trip) {
+            function(passengers) {
                 return trip.driverId === this.id;
             }.bind(this)
         );
